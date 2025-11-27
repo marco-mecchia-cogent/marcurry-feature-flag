f70eb7f6f23a4f81a75a8642039778b64ca23185
Marco Mecchia <marco-mecchia@cogent.co.jp>
2025-11-27 11:44:07 +0900

    chore: add tryCatch

--- services/web/lib/utils.ts
+++ services/web/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge';
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export async function tryCatch<T, E = Error>(promise: T | Promise<T>) {
+  try {
+    const data = await promise;
+    return [null, data] as const;
+  } catch (error) {
+    return [error as E, null] as const;
+  }
+}
